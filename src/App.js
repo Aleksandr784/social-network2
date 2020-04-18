f352523983f7f143a802f352dbd654a4fb91701a
unknown <digiprotect@gmail.com>
2020-04-18 15:19:41 +0300

    added to git

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './App.css';
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Profile from './Components/Profile';
+
+
+
+const App = () => {
+  return (
+    <div className='app-wraper'>
+      <Header />
+      <Navbar />
+      <Profile />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+
+        <img src='https://www.customlogoshop.com/wp-content/uploads/2017/04/Equi-Correct.jpg' alt="ligo"></img>
+
+    </header>
+}
+
+export default Header;
